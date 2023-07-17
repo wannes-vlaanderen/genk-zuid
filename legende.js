3d5a56f05b8290d995a5800c8501a039d03a73a8
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-17 11:21:39 +0200

    Update legende.js

--- legende.js
+++ legende.js
@@ -1,4 +1,4 @@
-const legendeConfig = {
+const legende = {
   "#e8da11": "Nieuw Bedrijventerrein",
   "#3d4f71": "Deelproject herstructurering",
   "#b0a8c2": "Open Ruimte",
@@ -11,7 +11,7 @@ class Legenda {
     this._map = map;
     this._container = document.createElement('div');
     this._container.className = 'mapboxgl-ctrl';
-    for (const [color, name] of Object.entries(legendeConfig)) {
+    for (const [color, name] of Object.entries(legende)) {
       this._container.appendChild(this.newEntry(color, name));
     }
     this._container.width = 120
