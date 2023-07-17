80d8eabae822e2eaf184eed817d4bf79df143c11
wannes-vlaanderen <138599016+wannes-vlaanderen@users.noreply.github.com>
2023-07-17 12:04:13 +0200

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -12,10 +12,6 @@ function createSidebar() {
   div1.id = "div1"
   const div2 = document.createElement("div")
   div2.classList.add('flex-child', 'w-full', 'w360-ml', 'absolute', 'static-ml', 'left', 'bottom')
-  const div3 = document.createElement("div")
-  div3.classList.add('flex-parent', 'flex-parent--column')
-  const div4 = document.createElement("div")
-  div4.classList.add('flex-child', 'flex-child--grow')
   const sidebar = document.createElement("div")
   sidebar.id = 'sidebarA'
   sidebar.classList.add('flex-parent', 'flex-parent--column-ml', 'flex-parent--center-main', 'py12', 'px12')
@@ -40,8 +36,6 @@ function createSidebar() {
   sidebar.appendChild(buttonDiv) // option to add button for filters here
   sidebar.appendChild(listing)
   
-  div4.appendChild(sidebar)
-  div3.appendChild(div4)
   div2.appendChild(sidebar)
   div1.appendChild(div2)
   
